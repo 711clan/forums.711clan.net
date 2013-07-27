8fded1f400645b234190321b761865179f93b3af
Website <clan711site@suva.dreamhost.com>
2013-07-26 23:38:33 -0700

    throwing together quick error handling

---
+++ error/404.php
@@ -0,0 +1,2 @@
+<p>Uh oh.  Looks like we couldn't find what you're looking for.</p>
+<p><a href="../">Back to Home Page</a></p>
---
+++ error/500.php
@@ -0,0 +1,3 @@
+<h1>Good job.  You broke it.</h1>
+<p>Looks like something went wrong while fulfilling your request.  Rest assured, a team of highly trained gerbils have been dispatched to address this issue.</p>
+
